66251b8384bc8884d412b006dbb5f42fa71e5aa6
wuzhihao-jk <wuzhihao-jk@360shuke.com>
2022-12-07 11:55:15 +0800

    caffeine

--- src/test/java/com/github/goodluckwu/onepiece/CaffeineTests.java
+++ src/test/java/com/github/goodluckwu/onepiece/CaffeineTests.java
@@ -1,5 +1,6 @@
 package com.github.goodluckwu.onepiece;
 
+import java.time.Duration;
 import java.time.LocalTime;
 import java.util.List;
 import java.util.Map;
@@ -201,14 +202,13 @@ public class CaffeineTests {
         System.out.println(cache.estimatedSize());
         System.out.println(cache.asMap());
 
-        // valuea.length() + valueb.length() > 2就会驱逐， 然后保留权重小的，驱逐最新的
         TimeUnit.SECONDS.sleep(5L);
         System.out.println(cache.estimatedSize());
         System.out.println(cache.asMap());
     }
 
     @Test
-    public void testEvictByCapacityByExpireAfterAccess() throws InterruptedException {
+    public void testEvictByExpireAfterAccess() throws InterruptedException {
         //  一个元素在上一次读写操作后一段时间之后，在指定的时间后没有被再次访问将会被认定为过期项。
         //  在当被缓存的元素时被绑定在一个session上时，当session因为不活跃而使元素过期的情况下，这是理想的选择。
         FakeTicker fakeTicker = new FakeTicker();
@@ -227,7 +227,7 @@ public class CaffeineTests {
     }
 
     @Test
-    public void testEvictByCapacityByExpireAfterWrite() throws InterruptedException {
+    public void testEvictByExpireAfterWrite() throws InterruptedException {
         //   一个元素将会在其创建或者最近一次被更新之后的一段时间后被认定为过期项。
         //   在对被缓存的元素的时效性存在要求的场景下，这是理想的选择。
         FakeTicker fakeTicker = new FakeTicker();
@@ -244,7 +244,7 @@ public class CaffeineTests {
     }
 
     @Test
-    public void testEvictByCapacityByExpireAfter() throws InterruptedException {
+    public void testEvictByExpireAfter() throws InterruptedException {
         // 基于不同的过期驱逐策略
         FakeTicker fakeTicker = new FakeTicker();
         LoadingCache<String, String> cache = Caffeine.newBuilder()
@@ -326,5 +326,180 @@ public class CaffeineTests {
         System.out.println(cache.asMap());
         fakeTicker.advance(10L, TimeUnit.SECONDS);
         System.out.println(cache.asMap());
+    }
+
+    @Test
+    public void testPolicyMaximumSize() throws InterruptedException {
+        // 基于缓存内的元素个数进行驱逐
+        LoadingCache<String, String> cache = Caffeine.newBuilder()
+            .maximumSize(1L)
+            .build(key -> UUID.randomUUID().toString());
+        cache.put("a", "1");
+        System.out.println(cache.estimatedSize());
+        System.out.println(cache.asMap());
+
+        TimeUnit.SECONDS.sleep(1L);
+
+        cache.put("b", "1");
+        System.out.println(cache.estimatedSize());
+        System.out.println(cache.asMap());
+
+        // key个数大于1就会驱逐，然后保留最近一个
+        TimeUnit.SECONDS.sleep(5L);
+        System.out.println(cache.estimatedSize());
+        System.out.println(cache.asMap());
+
+        cache.policy().eviction().ifPresent(eviction -> {
+            System.out.println("policy: " + eviction.getMaximum());
+            System.out.println("policy: " + eviction.weightedSize());
+            System.out.println("policy: " + cache.estimatedSize());
+            eviction.setMaximum(2 * eviction.getMaximum());
+        });
+
+        // key个数大于1就会驱逐，然后保留最近一个
+        cache.put("a", "1");
+        cache.put("c", "1");
+        System.out.println(cache.asMap());
+        TimeUnit.SECONDS.sleep(5L);
+        System.out.println(cache.estimatedSize());
+        System.out.println(cache.asMap());
+    }
+
+    @Test
+    public void testPolicyMaximumWeight() throws InterruptedException {
+        // 基于缓存内的元素个数进行驱逐
+        LoadingCache<String, String> cache = Caffeine.newBuilder()
+            .maximumWeight(1L)
+            .weigher((String key, String value) -> Integer.parseInt(value))
+            .build(key -> UUID.randomUUID().toString());
+        cache.put("a", "1");
+        System.out.println(cache.asMap());
+
+        TimeUnit.SECONDS.sleep(1L);
+
+        cache.put("b", "1");
+        System.out.println(cache.asMap());
+
+        TimeUnit.SECONDS.sleep(5L);
+        System.out.println(cache.asMap());
+
+        cache.policy().eviction().ifPresent(eviction -> {
+            System.out.println("policy: " + eviction.getMaximum());
+            System.out.println("policy: " + eviction.weightedSize());
+            System.out.println("policy: " + cache.estimatedSize());
+            eviction.setMaximum(2 * eviction.getMaximum());
+        });
+
+        cache.put("a", "1");
+        cache.put("c", "1");
+        System.out.println(cache.asMap());
+        TimeUnit.SECONDS.sleep(5L);
+        System.out.println(cache.asMap());
+        cache.policy().eviction().ifPresent(eviction -> {
+            System.out.println("policy: " + eviction.getMaximum());
+            System.out.println("policy: " + eviction.weightedSize());
+            System.out.println("policy: " + cache.estimatedSize());
+        });
+    }
+
+    @Test
+    public void testPolicyByExpireAfterAccess() throws InterruptedException {
+        //  一个元素在上一次读写操作后一段时间之后，在指定的时间后没有被再次访问将会被认定为过期项。
+        //  在当被缓存的元素时被绑定在一个session上时，当session因为不活跃而使元素过期的情况下，这是理想的选择。
+        FakeTicker fakeTicker = new FakeTicker();
+        LoadingCache<String, String> cache = Caffeine.newBuilder()
+            .expireAfterAccess(5L, TimeUnit.SECONDS)
+            .ticker(fakeTicker::read)
+            .build(key -> UUID.randomUUID().toString());
+
+        System.out.println(LocalTime.now() + ": " + cache.get("aaa"));
+        fakeTicker.advance(1L, TimeUnit.SECONDS);
+        System.out.println(LocalTime.now() + ": " + cache.get("aaa"));
+        fakeTicker.advance(3L, TimeUnit.SECONDS);
+        System.out.println(LocalTime.now() + ": " + cache.get("aaa"));
+        fakeTicker.advance(3L, TimeUnit.SECONDS);
+        System.out.println(LocalTime.now() + ": " + cache.get("aaa"));
+        fakeTicker.advance(5L, TimeUnit.SECONDS);
+        System.out.println(LocalTime.now() + ": " + cache.get("aaa"));
+        fakeTicker.advance(1L, TimeUnit.SECONDS);
+        cache.policy().expireAfterAccess().ifPresent(expiration -> {
+            System.out.println(cache.asMap());
+            System.out.println(expiration.ageOf("aaa"));
+            System.out.println(expiration.getExpiresAfter());
+            expiration.setExpiresAfter(Duration.ofSeconds(1));
+            System.out.println(expiration.getExpiresAfter());
+        });
+        System.out.println(LocalTime.now() + ": " + cache.get("aaa"));
+        fakeTicker.advance(1L, TimeUnit.SECONDS);
+        System.out.println(LocalTime.now() + ": " + cache.get("aaa"));
+        cache.policy().expireAfterAccess().ifPresent(expiration -> {
+            System.out.println(expiration.ageOf("aaa"));
+            System.out.println(expiration.getExpiresAfter());
+            expiration.setExpiresAfter(Duration.ofSeconds(5));
+            System.out.println(expiration.getExpiresAfter());
+        });
+    }
+
+    @Test
+    public void testPolicyByExpireAfterWrite() throws InterruptedException {
+        //   一个元素将会在其创建或者最近一次被更新之后的一段时间后被认定为过期项。
+        //   在对被缓存的元素的时效性存在要求的场景下，这是理想的选择。
+        FakeTicker fakeTicker = new FakeTicker();
+        LoadingCache<String, String> cache = Caffeine.newBuilder()
+            .expireAfterWrite(5L, TimeUnit.SECONDS)
+            .ticker(fakeTicker::read)
+            .build(key -> UUID.randomUUID().toString());
+
+        System.out.println(LocalTime.now() + ": " + cache.get("aaa"));
+        fakeTicker.advance(3L, TimeUnit.SECONDS);
+        System.out.println(LocalTime.now() + ": " + cache.get("aaa"));
+        fakeTicker.advance(3L, TimeUnit.SECONDS);
+        System.out.println(LocalTime.now() + ": " + cache.get("aaa"));
+
+        fakeTicker.advance(3L, TimeUnit.SECONDS);
+        cache.policy().expireAfterWrite().ifPresent(expiration -> {
+            System.out.println(expiration.getExpiresAfter());
+            System.out.println(expiration.ageOf("aaa"));
+        });
+    }
+
+    @Test
+    public void testPolicyByExpireAfter() throws InterruptedException {
+        // 基于不同的过期驱逐策略
+        FakeTicker fakeTicker = new FakeTicker();
+        LoadingCache<String, String> cache = Caffeine.newBuilder()
+            .expireAfter(new Expiry<String, String>() {
+                public long expireAfterCreate(String key, String value, long currentTime) {
+                    // 创建1秒后过期，可以看到这里必须要用纳秒
+                    System.out.printf("expireAfterCreate: %s:%s:%s%n", key, value, currentTime);
+                    return TimeUnit.SECONDS.toNanos(1);
+                }
+                public long expireAfterUpdate(String key, String value,
+                                              long currentTime, long currentDuration) {
+                    // 更新2秒后过期，可以看到这里必须要用纳秒
+                    System.out.printf("expireAfterUpdate: %s:%s:%s:%s%n", key, value, currentTime, currentDuration);
+                    return TimeUnit.SECONDS.toNanos(2);
+                }
+                public long expireAfterRead(String key, String value,
+                                            long currentTime, long currentDuration) {
+                    // 读3秒后过期，可以看到这里必须要用纳秒
+                    System.out.printf("expireAfterRead: %s:%s:%s:%s%n", key, value, currentTime, currentDuration);
+                    return TimeUnit.SECONDS.toNanos(3);
+                }
+            })
+            .ticker(fakeTicker::read)
+            .build(key -> UUID.randomUUID().toString());
+
+        System.out.println(LocalTime.now() + ": " + cache.get("aaa"));
+        cache.put("aaa", "111");
+        System.out.println(LocalTime.now() + ": " + cache.get("aaa"));
+        fakeTicker.advance(6L, TimeUnit.SECONDS);
+        System.out.println(LocalTime.now() + ": " + cache.get("aaa"));
+
+        cache.policy().expireVariably().ifPresent(expiration -> {
+            System.out.println(expiration.getExpiresAfter("aaa"));
+            expiration.put("aaa", "111", Duration.ofSeconds(5));
+            System.out.println(expiration.getExpiresAfter("aaa"));
+        });
     }
 }
