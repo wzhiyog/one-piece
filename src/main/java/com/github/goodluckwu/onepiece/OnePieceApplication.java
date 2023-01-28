aada05cfcbe331a979c1f45653e9c766440d630d
wuzhihao-jk <wuzhihao-jk@360shuke.com>
2023-01-28 11:15:14 +0800

    monitor

--- src/main/java/com/github/goodluckwu/onepiece/OnePieceApplication.java
+++ src/main/java/com/github/goodluckwu/onepiece/OnePieceApplication.java
@@ -2,12 +2,15 @@ package com.github.goodluckwu.onepiece;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
+import org.springframework.boot.web.context.WebServerPortFileWriter;
 
 @SpringBootApplication
 public class OnePieceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OnePieceApplication.class, args);
+		SpringApplication app = new SpringApplication(OnePieceApplication.class);
+		app.addListeners(new ApplicationPidFileWriter(), new WebServerPortFileWriter());
+		app.run(args);
 	}
-
 }
