27cc2d327006b9d70b91cfdba6c0cd1682f0d298
wuzhihao-jk <wuzhihao-jk@360shuke.com>
2023-01-28 09:47:56 +0800

    clean

--- src/test/java/com/github/goodluckwu/onepiece/tookit/mock/powermockito/TestService.java
+++
@@ -1,8 +0,0 @@
-package com.github.goodluckwu.onepiece.tookit.mock.powermockito;
-
-public class TestService {
-    public static Object newInstance2() {
-        System.out.println("TestService.newInstance2");
-        return new Object();
-    }
-}
