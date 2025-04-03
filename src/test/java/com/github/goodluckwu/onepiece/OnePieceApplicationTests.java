81bf45e4a05b7e3366c96a5e943d7ac1efb275e4
wuzhihao-jk <wuzhihao-jk@360shuke.com>
2025-04-03 15:11:00 +0800

    feat(arch): 异常处理

--- src/test/java/com/github/goodluckwu/onepiece/OnePieceApplicationTests.java
+++ src/test/java/com/github/goodluckwu/onepiece/OnePieceApplicationTests.java
@@ -1,5 +1,11 @@
 package com.github.goodluckwu.onepiece;
 
+import java.net.URL;
+
+import com.github.wzhiyog.core.exception.Error;
+import io.github.classgraph.ClassGraph;
+import io.github.classgraph.ClassInfoList;
+import io.github.classgraph.ScanResult;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
@@ -9,28 +15,33 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
-import java.net.URL;
-
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class OnePieceApplicationTests {
-	private static final Logger log = LoggerFactory.getLogger(OnePieceApplicationTests.class);
+    private static final Logger log = LoggerFactory.getLogger(OnePieceApplicationTests.class);
 
-	@LocalServerPort
-	private int port;
+    @LocalServerPort
+    private int port;
 
-	private URL base;
+    private URL base;
 
-	@Autowired
-	private TestRestTemplate restTemplate;
+    @Autowired
+    private TestRestTemplate restTemplate;
 
-	@BeforeEach
-	public void setUp() throws Exception {
-		this.base = new URL("http://localhost:" + port + "/hello1?name=tracer");
-	}
+    @BeforeEach
+    public void setUp() throws Exception {
+        this.base = new URL("http://localhost:" + port + "/hello1?name=tracer");
+    }
 
-	@Test
-	void contextLoads() {
-		log.info("{}", restTemplate.getForEntity(base.toString(), String.class));
-	}
+    @Test
+    void contextLoads() {
+        log.info("{}", restTemplate.getForEntity(base.toString(), String.class));
+    }
+
+    public static void main(String[] args) {
+        try (ScanResult scanResult = new ClassGraph().verbose().enableAllInfo().acceptPackages("com.github.wzhiyog").scan()) {
+            ClassInfoList classesImplementing = scanResult.getClassesImplementing(Error.class);
+            classesImplementing.forEach(System.out::println);
+        }
+    }
 
 }
