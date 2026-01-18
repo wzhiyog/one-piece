038f0fe6681ff0f3fa53aa92b753dfd05c66d6d7
wuzhiyong <wuzhihao7@outlook.com>
2026-01-18 10:37:18 +0800

    PDF

--- spring-boot-learning/src/main/java/com/example/spring/boot/learning/agreement/ContractService.java
+++ spring-boot-learning/src/main/java/com/example/spring/boot/learning/agreement/ContractService.java
@@ -31,13 +31,13 @@ import java.util.*;
 @RequiredArgsConstructor
 @Slf4j
 @Component
-public class FillService {
+public class ContractService {
     private final DataLoaderFactory dataLoaderFactory;
     private final ExpressionParser parser = new SpelExpressionParser();
 
-    public byte[] fillPdf(ContractReq contractReq) {
+    public byte[] createContract(ContractReq contractReq) {
         // 获取填充项
-        List<FillItem> fillItemList = contractReq.getFillItemList();
+        List<FillItem> fillItemList = getFillItemList(contractReq);
 
         // 加载数据源
         List<Integer> dataLoaderList = fillItemList.stream().map(FillItem::getDataLoader).distinct().toList();
@@ -51,6 +51,27 @@ public class FillService {
 
         // 填充pdf
         return fillPdf(formData, pdf);
+    }
+
+    private List<FillItem> getFillItemList(ContractReq contractReq) {
+        FillItem fillItem = new FillItem();
+        fillItem.setItemName("NameB");
+        fillItem.setExpression("date");
+        fillItem.setFillType(FillTypeEnum.EXPRESSION.getCode());
+        fillItem.setDataLoader(DataLoaderEnum.DATA1.getCode());
+        fillItem.setOrder(1);
+        fillItem.setFormatter(FormatterEnum.DATETIME.getCode());
+        fillItem.setFormatPattern("yyyy-MM-dd HHmmss");
+
+        FillItem fillItem2 = new FillItem();
+        fillItem2.setItemName("NameA");
+        fillItem2.setExpression("numberString");// map要用中括号
+        fillItem2.setFillType(FillTypeEnum.EXPRESSION.getCode());
+        fillItem2.setDataLoader(DataLoaderEnum.DATA1.getCode());
+        fillItem2.setOrder(2);
+        fillItem2.setFormatter(FormatterEnum.NUMBER.getCode());
+        fillItem2.setFormatPattern("¤#.##");
+        return Arrays.asList(fillItem, fillItem2);
     }
 
     private byte[] loadPdf() {
--- spring-boot-learning/src/test/java/com/example/spring/boot/learning/SpringBootLearningApplicationTests.java
+++ spring-boot-learning/src/test/java/com/example/spring/boot/learning/SpringBootLearningApplicationTests.java
@@ -1,6 +1,7 @@
 package com.example.spring.boot.learning;
 
-import com.example.spring.boot.learning.agreement.*;
+import com.example.spring.boot.learning.agreement.ContractReq;
+import com.example.spring.boot.learning.agreement.ContractService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,36 +9,16 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
 
 @SpringBootTest
 class SpringBootLearningApplicationTests {
     @Autowired
-    private FillService fillService;
+    private ContractService contractService;
 
     @Test
     void contextLoads() throws IOException {
-        ContractReq contractReq = new ContractReq();
-        FillItem fillItem = new FillItem();
-        fillItem.setItemName("NameB");
-        fillItem.setExpression("date");
-        fillItem.setFillType(FillTypeEnum.EXPRESSION.getCode());
-        fillItem.setDataLoader(DataLoaderEnum.DATA1.getCode());
-        fillItem.setOrder(1);
-        fillItem.setFormatter(FormatterEnum.DATETIME.getCode());
-        fillItem.setFormatPattern("yyyy-MM-dd HHmmss");
 
-        FillItem fillItem2 = new FillItem();
-        fillItem2.setItemName("NameA");
-        fillItem2.setExpression("numberString");// map要用中括号
-        fillItem2.setFillType(FillTypeEnum.EXPRESSION.getCode());
-        fillItem2.setDataLoader(DataLoaderEnum.DATA1.getCode());
-        fillItem2.setOrder(2);
-        fillItem2.setFormatter(FormatterEnum.NUMBER.getCode());
-        fillItem2.setFormatPattern("¤#.##");
-        contractReq.setFillItemList(Arrays.asList(fillItem, fillItem2));
-
-        byte[] bytes = fillService.fillPdf(contractReq);
+        byte[] bytes = contractService.createContract(new ContractReq());
         Files.write(Paths.get("C:\\Users\\wuzhi\\Desktop\\loan-agreement-template-zh-fill.pdf"), bytes);
     }
 
